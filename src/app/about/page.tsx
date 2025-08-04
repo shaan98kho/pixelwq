d3f8210f46f11607e0c583d67b76dcd773ed0808
Wen Qi Kho <shaankho1998@gmail.com>
2025-08-04 23:52:20 +0800

    feat: added content for project page and refactored About page tech stack renderring logic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,31 @@
 "use client"
 
 import Image from 'next/image'
+import dynamic from "next/dynamic"
 import { useRef } from 'react'
-import VariableProximity from '@/components/VariableProximity'
+const VariableProximity = dynamic(() => import("@/components/VariableProximity"), { ssr: false });
 
 
 export default function About() {
     const containerRef = useRef(null)
+    const feStack: string[] = ['javascript', 'typescript', 'react', 'nextjs', 'flutter', 'redux', 'sass', 'tailwindcss', 'bootstrap', 'html5', 'jquery']
+    const beStack: string[] = ['firebase', 'java', 'python', 'mysql', 'php']
+    const tools: string[] = ['git', 'gitlab', 'figma', 'jira', 'github']
+    // const testingTools: string[] = ['jest']
 
+    const generateIcons = (arr: string[]) => {
+        return arr.map((item, idx) => <Image width={40} height={40} src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${item}/${item}-original.svg`} alt={`${item} logo`}  key={`${item}${idx}`}/>)
+    }
+    const feIcons = generateIcons(feStack)
+    const beIcons = generateIcons(beStack)
+    const toolsIcons = generateIcons(tools)
+    const testingIcons = <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"  alt="jest logo"  />
     return <div>
-        <p className='text-center text-sm'>I build responsive, user-centered web apps with</p>
+        <p className='text-center text-sm inline'>I build responsive, user-centered web apps with</p>
          <div
 			ref={containerRef}
 			style={{position: 'relative'}}
-            className='variable-proximity text-center'
+            className='variable-proximity text-center inline px-1'
 			>
 				<VariableProximity
 					label={'React.js, Next.js, TypeScript, Zustand & Firebase'}
@@ -25,46 +37,24 @@ export default function About() {
 					falloff='gaussian'
 				/>
 			</div>
-        <p className='text-center text-sm'>while optimizing performance, accessibility, and clean architecture along the way. With a background in UI development and 1 year of hands-on experience translating Figma designs into scalable interfaces (HTML, SCSS, JavaScript & jQuery), I thrive on collaborating across disciplines and continuously strengthening my core JavaScript fundamentals.</p>
-        <div className='flex flex-col align-center justify-center'>            
+        <p className='text-center text-sm inline'>while optimizing performance, accessibility, and clean architecture along the way. With a background in UI development and 1 year of hands-on experience translating Figma designs into scalable interfaces (HTML, SCSS, JavaScript & jQuery), I thrive on collaborating across disciplines and continuously strengthening my core JavaScript fundamentals.</p>
+        <div className='flex flex-col align-center justify-center pt-4 font-bold'>
             <h2 className='text-lg pb-4'>Tech Stack</h2>
             <h4 className='text-lg pb-2'>Front End:</h4>
-            <div className="flex gap-2 pb-6">
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript logo"  />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" alt="nextjs logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" alt="flutter logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="redux logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt="sass logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="tailwindcss logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt="bootstrap logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html5 logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg" alt="jquery logo"   />
-            </div>
-
-            
+            <div className="flex gap-2 pb-6 flex-wrap">
+                {feIcons}
+            </div>            
             <h4 className='text-lg pb-2'>Back End/Database:</h4>
-            <div className="flex gap-2 pb-6">
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" alt="firebase logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="java logo"   />            
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="python logo"   />            
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" alt="php logo"   />
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="mysql logo"   />
-            </div>
-            
+            <div className="flex gap-2 pb-6 flex-wrap">
+                {beIcons}
+            </div>            
             <h4 className='text-lg pb-2'>Tools:</h4>
-            <div className="flex gap-2 pb-6">
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"   alt="git logo"  />            
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg"  alt="gitlab logo"  />            
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"  alt="figma logo"  />            
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg"  alt="jira logo"  />            
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"  alt="github logo"  />
-            </div>
-            
+            <div className="flex gap-2 pb-6 flex-wrap">
+                {toolsIcons}
+            </div>            
             <h4 className='text-lg pb-2'>Testing:</h4>
-            <div className="flex gap-2">
-                <Image width={40} height={40} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"  alt="jest logo"  />
+            <div className="flex gap-2 flex-wrap">
+                {testingIcons}
             </div>
         </div>
     </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,78 @@
+"use client"
+
+import dynamic from "next/dynamic"
+import Link from "next/link";
+const TiltedCard = dynamic(() => import("@/components/TiltedCard"), { ssr: false });
+
 export default function Projects() {
-    return <>This is Projects</>
+    
+    return <div>
+        This page is still under construction...
+        <div className="projects-wrap pt-4 flex flex-wrap align-center gap-6">
+            <a target="_blank" href="https://herobite.vercel.app/">
+                <TiltedCard
+                    imageSrc="../../../../herobiteSS.png"
+                    altText="HeroBite"
+                    captionText="HeroBite live site"
+                    containerHeight="200px"
+                    containerWidth="200px"
+                    imageHeight="200px"
+                    imageWidth="200px"
+                    rotateAmplitude={12}
+                    scaleOnHover={1.2}
+                    showMobileWarning={false}
+                    showTooltip={true}
+                    displayOverlayContent={true}
+                    overlayContent={
+                        <p className="tilted-card-demo-text text-xs">
+                            Hero Bite
+                        </p>
+                    }
+                />
+            </a>
+            <a target="_blank" href="https://movies-watch-list-lime.vercel.app/">
+                <TiltedCard
+                    imageSrc="../../../../moviesWatchListSS.png"
+                    altText="HeroBite"
+                    captionText="Movies Watchlist live site"
+                    containerHeight="200px"
+                    containerWidth="200px"
+                    imageHeight="200px"
+                    imageWidth="200px"
+                    rotateAmplitude={12}
+                    scaleOnHover={1.2}
+                    showMobileWarning={false}
+                    showTooltip={true}
+                    displayOverlayContent={true}
+                    overlayContent={
+                        <p className="tilted-card-demo-text text-xs">
+                            Movies Watchlist
+                        </p>
+                    }
+                />
+            </a>
+            <a target="_blank" href="https://wq-quizzical.netlify.app/">
+                <TiltedCard
+                    imageSrc="../../../../quizzicalSs.png"
+                    altText="Quizzical App"
+                    captionText="Trivial Quiz live site"
+                    containerHeight="200px"
+                    containerWidth="200px"
+                    imageHeight="200px"
+                    imageWidth="200px"
+                    rotateAmplitude={12}
+                    scaleOnHover={1.2}
+                    showMobileWarning={false}
+                    showTooltip={true}
+                    displayOverlayContent={true}
+                    overlayContent={
+                        <p className="tilted-card-demo-text text-xs">
+                            Quizzical App
+                        </p>
+                    }
+                />
+            </a>
+
+        </div>
+    </div>
 }
