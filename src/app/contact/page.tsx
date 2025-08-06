5d611ed39cc7a8c1c8d82019334e014ed0685dfe
Wen Qi Kho <shaankho1998@gmail.com>
2025-08-06 23:33:01 +0800

    feat: added contact form. emailing functionalities TBA

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,47 @@
+"use client"
+
+import { useState } from "react"
 import { FaGithub, FaLinkedin } from "react-icons/fa"
+import ClickSpark from "@/components/ClickSpark"
 
 export default function Contact() {
-    return <div>This page is still under construction
-        <div className="flex gap-2 text-4xl">
+    const [name, setName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [message, setMessage] = useState<string>("")
+
+    const colours = ["#7cff67", "#b19eef", "#5227ff"]
+
+    const generateColour = () => {
+        const randomIndex = Math.floor(Math.random() * colours.length);
+        console.log(randomIndex)
+        return colours[randomIndex];
+    }
+
+
+    return <div>
+        {/* <p className="">Get in touch with me at: </p> */}
+        <div className="flex gap-2 text-4xl pb-4">
             <a target="_blank" href="https://github.com/shaan98kho/herobite" rel="noopener noreferrer"
             ><FaGithub/></a>
             <a target="_blank" href="https://www.linkedin.com/in/wen-qi-k-966581138/" rel="noopener noreferrer"><FaLinkedin/></a>
         </div>
+        <p>Or send me a message</p>
+        <ClickSpark
+        sparkColor="rgba(255,255,255,0.6)"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+        >
+            
+            <form method="POST" className="form flex flex-col gap-4 pt-4">
+                <input type="text" name="name" placeholder="Your Name" required />
+                <input type="email" name="email" placeholder="you@example.com" required />
+                <textarea name="message" rows={5} placeholder="How can I help you?" className="resize-none" required />
+                <button>Send</button>
+            </form>
+        </ClickSpark>
+        
 
     </div>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,1 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//     content: [
-//       './app/**/*.{js,jsx,ts,tsx}',
-//       './components/**/*.{js,jsx,ts,tsx}',
-//       './node_modules/@react-bits/text-animations/variable-proximity/**/*.{js,ts,jsx,tsx}',
-//     ],
-//     theme: { extend: {} },
-//     plugins: [
-//       require('@react-bits/text-animations/variable-proximity/plugin'),
-//     ],
-//   }
-  
+/** @type {import('tailwindcss').Config} */
