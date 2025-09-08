934df2e665fe3957ec97bf88049b94e54769b89b
Wen Qi Kho <shaankho1998@gmail.com>
2025-09-08 21:35:29 +0800

    feat: fixed intro content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 			<br/>
 			<ul className=" flex flex-col gap-2 pb-4">
 				<li>When I'm not coding you'll probably find me behind a drum kit<br/>I picked it up a few months ago and I love how it keeps my mind sharp and my wrists strong.</li>
-				<li>I also do aerial yoga every week, balancing myself while being upside-down on a silk hammock has taught me to stay calm under pressure<br/>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</li>
+				<li>I also do aerial yoga every week, balancing myself while being upside-down on a silk hammock has taught me to stay calm under pressure</li>
 				<li>And I love to draw too! My passion in art lies in human portraits. This forces me to slow down and see the world in new ways.</li> 
 			</ul>
 			<p>Those three things:</p>
