05ab95650737a9dfd13abf30e7ef5bc4e1022e0c
Wen Qi Kho <shaankho1998@gmail.com>
2025-12-16 18:23:24 +0800

    - updated about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 			<h2 className="text-2xl">I'm a frontend dev from Malaysia who cares deeply about clean UI and thoughtful interactions.</h2>
 			<br/>
 			<ul className=" flex flex-col gap-2 pb-4">
-				<li>When I'm not coding you'll probably find me behind a drum kit<br/>I picked it up a few months ago and I love how it keeps my mind sharp and my wrists strong.</li>
+				<li>When I'm not coding you'll probably find me behind a drum kit,<br/>I picked it up a few months ago and I love how it keeps my mind sharp and my wrists strong.<br/>You can follow my drumming journey <a href="https://open.spotify.com/playlist/6HMbKdCgYOQi4JJapks6D0?si=7dcf45e87322443d" target="_blank">HERE</a> C:</li>
 				<li>I also do aerial yoga every week, balancing myself while being upside-down on a silk hammock has taught me to stay calm under pressure</li>
 				<li>And I love to draw too! My passion in art lies in human portraits. This forces me to slow down and see the world in new ways.</li> 
 			</ul>
